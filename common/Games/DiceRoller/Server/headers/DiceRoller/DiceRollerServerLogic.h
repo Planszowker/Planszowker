88a652ac74c4f9a5cc2b38bcd1f4fcbdfca4f0d5
Dawid Tobor <dawidtobor@op.pl>
2021-02-24 21:27:18 +0100

    WIP

--- common/Games/DiceRoller/Server/headers/DiceRoller/DiceRollerServerLogic.h
+++ common/Games/DiceRoller/Server/headers/DiceRoller/DiceRollerServerLogic.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include "NetworkHandler/NetworkHandler.h"
+
+#include <memory>
+
+namespace pla::common::dice_roller {
+
 class DiceRollerServerLogic
+{
+public:
+  DiceRollerServerLogic();
+
+private:
+  std::shared_ptr<network::NetworkHandler> m_networkHandler;
+
+};
+
+}
